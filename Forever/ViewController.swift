c3b80f8f889985d18c0ab6fe2f9412a135d6d68d
오진성 <leonard@toss.im>
2019-11-28 11:27:51 +0900

    init:

--- Forever/ViewController.swift
+++ Forever/ViewController.swift
@@ -7,12 +7,87 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
+
+    let button1: UIButton = UIButton(type: .custom)
+    let button2: UIButton = UIButton(type: .custom)
+
+    let button3: UIButton = UIButton(type: .custom)
+    let button4: UIButton = UIButton(type: .custom)
+
+    let label1: UILabel = UILabel()
+    let label2: UILabel = UILabel()
+
+    var intervalDisposeBag1: DisposeBag = DisposeBag()
+    var intervalDisposeBag2: DisposeBag = DisposeBag()
+    var disposeBag: DisposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        view.addSubview(button1)
+        view.addSubview(button2)
+        view.addSubview(button3)
+        view.addSubview(button4)
+        view.addSubview(label1)
+        view.addSubview(label2)
+
+        button1.frame = CGRect(x: 100, y: 50, width: 200, height: 50)
+        button2.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
+        button3.frame = CGRect(x: 100, y: 150, width: 200, height: 50)
+        button4.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
+        button1.setTitleColor(.black, for: .normal)
+        button2.setTitleColor(.black, for: .normal)
+        button3.setTitleColor(.black, for: .normal)
+        button4.setTitleColor(.black, for: .normal)
+
+        button1.setTitle("1 연결", for: .normal)
+        button2.setTitle("1 끊기", for: .normal)
+        button3.setTitle("2 연결", for: .normal)
+        button4.setTitle("2 끊기", for: .normal)
+
+        label1.frame = CGRect(x: 100, y: 300, width: 200, height: 50)
+        label2.frame = CGRect(x: 100, y: 400, width: 200, height: 50)
+
+        label1.textColor = .black
+        label2.textColor = .black
+
+        rxInit()
+    }
+
+    func rxInit() {
+
+        let interval = Observable<Int>
+            .interval(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance)
+            .startWith(-7)
+            .map { "\($0)" }
+            .share(replay: 1, scope: .forever)
+
+        button1.rx.tap
+            .subscribe(onNext: { _ in
+                interval.bind(to: self.label1.rx.text).disposed(by: self.intervalDisposeBag1)
+            }).disposed(by: disposeBag)
+
+        button2.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+                self?.intervalDisposeBag1 = DisposeBag()
+                self?.label1.text = ""
+            }).disposed(by: disposeBag)
+
+        button3.rx.tap
+            .subscribe(onNext: { _ in
+                interval.bind(to: self.label2.rx.text).disposed(by: self.intervalDisposeBag2)
+            }).disposed(by: disposeBag)
+
+        button4.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+                self?.intervalDisposeBag2 = DisposeBag()
+                self?.label2.text = ""
+            }).disposed(by: disposeBag)
     }
 
 
---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Forever' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Forever
+  pod 'RxSwift'
+  pod 'RxCocoa'
+
+  target 'ForeverTests' do
+    inherit! :search_paths
+    # Pods for testing
+
+  end
+
+end
