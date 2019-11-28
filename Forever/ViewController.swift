8645bd0189edd3bbd731ec99660ea205a1ea9156
오진성 <leonard@toss.im>
2019-11-28 11:40:41 +0900

    forever, whileconnected 선택 가능기능 추가.

--- Forever/ViewController.swift
+++ Forever/ViewController.swift
@@ -21,6 +21,8 @@ class ViewController: UIViewController {
     let label1: UILabel = UILabel()
     let label2: UILabel = UILabel()
 
+    let segment: UISegmentedControl = UISegmentedControl(items: ["whileConnected", "forever"])
+
     var intervalDisposeBag1: DisposeBag = DisposeBag()
     var intervalDisposeBag2: DisposeBag = DisposeBag()
     var disposeBag: DisposeBag = DisposeBag()
@@ -35,11 +37,14 @@ class ViewController: UIViewController {
         view.addSubview(button4)
         view.addSubview(label1)
         view.addSubview(label2)
+        view.addSubview(segment)
 
-        button1.frame = CGRect(x: 100, y: 50, width: 200, height: 50)
-        button2.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
-        button3.frame = CGRect(x: 100, y: 150, width: 200, height: 50)
-        button4.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
+        segment.frame = CGRect(x: 0, y: 100, width: 300, height: 50)
+        segment.center.x = view.center.x
+        button1.frame = CGRect(x: 100, y: 150, width: 200, height: 50)
+        button2.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
+        button3.frame = CGRect(x: 100, y: 250, width: 200, height: 50)
+        button4.frame = CGRect(x: 100, y: 300, width: 200, height: 50)
         button1.setTitleColor(.black, for: .normal)
         button2.setTitleColor(.black, for: .normal)
         button3.setTitleColor(.black, for: .normal)
@@ -50,26 +55,40 @@ class ViewController: UIViewController {
         button3.setTitle("2 연결", for: .normal)
         button4.setTitle("2 끊기", for: .normal)
 
-        label1.frame = CGRect(x: 100, y: 300, width: 200, height: 50)
+        label1.frame = CGRect(x: 100, y: 350, width: 200, height: 50)
         label2.frame = CGRect(x: 100, y: 400, width: 200, height: 50)
 
         label1.textColor = .black
         label2.textColor = .black
+
+        segment.selectedSegmentIndex = 0
 
         rxInit()
     }
 
     func rxInit() {
 
-        let interval = Observable<Int>
+        let intervalWhileConnected = Observable<Int>
             .interval(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance)
-            .startWith(-7)
+            .map { "\($0)" }
+            .share(replay: 1, scope: .whileConnected)
+
+        let intervalForever = Observable<Int>
+            .interval(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance)
             .map { "\($0)" }
             .share(replay: 1, scope: .forever)
 
         button1.rx.tap
             .subscribe(onNext: { _ in
-                interval.bind(to: self.label1.rx.text).disposed(by: self.intervalDisposeBag1)
+                switch self.segment.selectedSegmentIndex {
+                case 0:
+                    intervalWhileConnected.bind(to: self.label1.rx.text).disposed(by: self.intervalDisposeBag1)
+                case 1:
+                    intervalForever.bind(to: self.label1.rx.text).disposed(by: self.intervalDisposeBag1)
+                default:
+                    break
+                }
+
             }).disposed(by: disposeBag)
 
         button2.rx.tap
@@ -80,7 +99,14 @@ class ViewController: UIViewController {
 
         button3.rx.tap
             .subscribe(onNext: { _ in
-                interval.bind(to: self.label2.rx.text).disposed(by: self.intervalDisposeBag2)
+                switch self.segment.selectedSegmentIndex {
+                case 0:
+                    intervalWhileConnected.bind(to: self.label2.rx.text).disposed(by: self.intervalDisposeBag2)
+                case 1:
+                    intervalForever.bind(to: self.label2.rx.text).disposed(by: self.intervalDisposeBag2)
+                default:
+                    break
+                }
             }).disposed(by: disposeBag)
 
         button4.rx.tap
